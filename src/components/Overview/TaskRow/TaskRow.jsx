368b5e553613694de5722b48ac730b452125513f
Drake Morrison <drakermorrison@gmail.com>
2019-01-26 13:53:04 -0600

    wip

--- src/components/Overview/TaskRow/TaskRow.jsx
+++ src/components/Overview/TaskRow/TaskRow.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class TaskRow extends React.Component {
   render() {
     return (
-      <div className='TaskRow panel panel-default'>
+      <div className='TaskRow panel panel-danger'>
         <div className='panel-body'>
           <h1>TaskRow</h1>
           {this.props.task.name}
