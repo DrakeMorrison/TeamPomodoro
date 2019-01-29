a8d2dd42f58847544c6661760437e07e2695911d
Drake Morrison <drakermorrison@gmail.com>
2019-01-28 19:48:34 -0600

    wip

--- src/components/Overview/ActiveInventoryColumn/ActiveInventoryColumn.jsx
+++ src/components/Overview/ActiveInventoryColumn/ActiveInventoryColumn.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import TaskRow from '../TaskRow/TaskRow';
+import { Droppable } from 'react-beautiful-dnd';
 
 export default class ActiveInventoryColumn extends React.Component {
   render() {
 
     const listOfTasks = this.props.tasks.map(task => {
-        return <TaskRow key={task.id} task={task} />
+        return <TaskRow key={task.id} task={task} index={task.id} />
+    });
+
+    const getListStyle = isDraggingOver => ({
+      background: isDraggingOver ? 'lightblue' : 'lightgrey',
+      padding: '8px',
+      width: 250,
     });
 
     return (
-      <div className='ActiveInventoryColumn bg-danger col-sm-6 col-md-4 col-lg-3'>
+      <Droppable droppableId='droppable' className='ActiveInventoryColumn bg-danger col-sm-6 col-md-4 col-lg-3'>
         <h2>ActiveInventory</h2>
-        {listOfTasks}
-      </div>
+
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            style={getListStyle(snapshot.isDraggingOver)}
+          >
+            {listOfTasks}
+            {provided.placeholder}
+
+          </div>
+        )}
+
+      </Droppable>
     );
   }
 }
--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import ActiveInventoryColumn from './ActiveInventoryColumn/ActiveInventoryColumn';
 import TodayColumnList from './TodayColumnList/TodayColumnList';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 export default class Overview extends React.Component {
+  state={
+    items: [],
+    selected: []
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items,
+    });
+  }
+
+
   render() {
     return (
       <div className='Overview row'>
 
-        <ActiveInventoryColumn tasks={this.props.initialData.tasks}/>
-        <TodayColumnList data={this.props.initialData}/>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+
+          <ActiveInventoryColumn tasks={this.props.initialData.tasks}/>
+          <TodayColumnList data={this.props.initialData}/>
+
+        </DragDropContext>
 
       </div>
     );
