79c1e703e4ef0b180e811a082b9a8b722461d114
Drake Morrison <drakermorrison@gmail.com>
2019-02-08 13:04:30 -0600

    wip

--- src/components/RecordsList/TaskRecord/TaskRecord.jsx
+++ src/components/RecordsList/TaskRecord/TaskRecord.jsx
@@ -6,7 +6,8 @@ export default class TaskRecord extends React.Component {
       <div className='TaskRecord panel panel-danger'>
         <div className='panel-body'>
           <h2>{this.props.task.name}</h2>
-          <h3>Time: {this.props.record.temporalResonance}</h3>
+          <h3>Start Time: {this.props.record.startTime}</h3>
+          <h3>End Time: {this.props.record.endTime}</h3>
         </div>
       </div>
     );
