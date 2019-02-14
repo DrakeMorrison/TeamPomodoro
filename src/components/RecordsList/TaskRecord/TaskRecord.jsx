cecf54a983696481f91d42e0983ccd7e6aadf165
Drake Morrison <drakermorrison@gmail.com>
2019-02-14 13:15:00 -0600

    records styled

--- src/components/RecordsList/TaskRecord/TaskRecord.jsx
+++ src/components/RecordsList/TaskRecord/TaskRecord.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 export default class TaskRecord extends React.Component {
   render() {
+    const startTime = new Date(`${this.props.record.startTime}Z`);
+    const endTime = new Date(`${this.props.record.endTime}Z`);
+    const options = { weekday: 'short', month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' };
+
     return (
       <div className='TaskRecord panel panel-danger'>
         <div className='panel-body'>
-          <h2>{this.props.task.name}</h2>
-          <h3>Start Time: {this.props.record.startTime}</h3>
-          <h3>End Time: {this.props.record.endTime}</h3>
+          <h3>{this.props.task.name}</h3>
+          <p>Created: {startTime.toLocaleDateString(undefined, options)}</p>
+          <p>Finished: {endTime.toLocaleDateString(undefined, options)}</p>
         </div>
       </div>
     );
